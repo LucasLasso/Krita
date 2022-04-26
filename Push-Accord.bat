6916141a41b7331fd778aeac58ee4a68fe5ca36b
Lucas Amorim <lucasamorim@accord.local>
2022-04-26 11:56:34 -0300

    Novo versionamento 26042022

---
+++ Push-Accord.bat
@@ -0,0 +1,23 @@
+@echo off
+cd C:\Users\lucasamorim\Downloads\Krita
+set filedatetime=%date% 
+set filedatetime=%filedatetime:~0,2%%filedatetime:~3,2%%filedatetime:~6,4%
+@echo on
+git status
+@echo off
+timeout 2 > nul
+@echo on
+git add .
+@echo off
+timeout 2 > nul
+@echo on
+git commit -m "Novo versionamento %filedatetime%"
+@echo off
+timeout 2 > nul
+@echo on
+git push
+echo 'Fim do push... '
+@echo off
+timeout 2 > nul
+@echo on
+pause
