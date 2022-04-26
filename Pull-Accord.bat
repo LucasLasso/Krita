9907fe0f7063370a6a7c7df7880f25e80d546924
Lucas Amorim <lucasamorim@accord.local>
2022-04-26 12:14:43 -0300

    Novo versionamento 26042022

---
+++ Pull-Accord.bat
@@ -0,0 +1,13 @@
+@echo off
+cd C:\Users\lucasamorim\Downloads\Krita
+@echo on
+git status
+@echo off
+timeout 2 > nul
+@echo on
+git pull
+echo 'Fim do pull... '
+@echo off
+timeout 2 > nul
+@echo on
+pause
